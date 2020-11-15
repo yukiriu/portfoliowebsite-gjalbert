e64d32067f639f2596d25bcf114e73d2ed367861
yukiriu <gabrieljalbert@hotmail.ca>
2020-11-15 16:51:14 -0500

    Remade landing page, added About Me page, started on Projects page

---
+++ tailwind.config.js
@@ -0,0 +1,29 @@
+module.exports = {
+  future: {
+    // removeDeprecatedGapUtilities: true,
+    // purgeLayersByDefault: true,
+  },
+  purge: [],
+  theme: {
+    extend: {
+      fontSize: {
+        '7xl': '5rem',
+        '8xl': '6rem',
+        '9xl': '7rem',
+        '10xl': '8rem',
+        '11xl': '9rem',
+        '12xl': '10rem',
+        '13xl': '11rem',
+  
+      },
+      spacing: {
+        '72': '18rem',
+        '84': '21rem',
+        '96': '24rem',
+        '128': '32rem'
+      }
+    },
+  },
+  variants: {},
+  plugins: [],
+}
